5d0677bd884aec68754037e62779882d6ef60f33
Rustam Yuburov <rt138328@gmail.com>
2021-07-29 19:52:14 +0600

    Finished Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import logo from '../assets/pokemon-logo.png'
 
 const Header = () => {
   return (
-    <div>
-      <h1>This is header</h1>
-    </div>
+    <HeaderWrapper>
+      <h1>Memory Cards with</h1>
+      <Logo src={logo} alt='logo'></Logo>
+    </HeaderWrapper>
   )
 }
 
 export default Header;
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  background-color: #e83d3a;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  font-size: 1.1rem;
+  font-family: 'Notable', sans-serif;
+  color: #fffc40;
+  text-shadow: 5px 5px 1px #4287ff;
+  // height: 50px;
+
+`
+const Logo = styled.img`
+  max-width: 300px;
+  margin-left: 20px;
+  // max-height: 500px;
+`
