6bb1336bf358b1c32206824d8d043f0e00913607
Rustam Yuburov <rt138328@gmail.com>
2021-07-29 22:53:11 +0600

    Added states and styled a bit component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import Scoreboard  from './Scoreboard';
+import CardGrid from './Cards/CardGrid';
+import styled from 'styled-components';
+import background from '../assets/background-img.png'
 
 const Main = () => {
+  // const POKEMONS_AMOUNT = 12;
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [pokemons, setPokemons] = useState([]);
+  // const [clickedPokemons, setClickedPokemons] = useState([]);
+  // const [currentScore, setCurrentScore] = useState(0);
+  // const [bestScore, setBestScore] = useState(0);
+
   return (
-    <div>
-      <h1>This is Main</h1>
-    </div>
+    <MainWrapper>
+      <Scoreboard />
+      <CardGrid />
+    </MainWrapper>
   )
 }
 
 export default Main;
+
+const MainWrapper = styled.div`
+  border: 1px solid #000;
+  background-image: url(${background});
+  width: 100%;
+  height: 1000px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
