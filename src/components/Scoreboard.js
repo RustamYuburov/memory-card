e47434adf298357bb75d1b6fe43aee2f13ce2569
Rustam Yuburov <rt138328@gmail.com>
2021-07-30 21:09:59 +0600

    Reworked scoreboard into glass style

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -5,10 +5,10 @@ const Scoreboard = (props) => {
   return(
     <ScoreboardWrapper>
       <ScoreWrapper>
-        Current score: 50
+        Current score: {props.currentScore}
       </ScoreWrapper>
       <ScoreWrapper>
-        Best score: 50
+        Best score: {props.bestScore}
       </ScoreWrapper>
     </ScoreboardWrapper>
   )
@@ -27,9 +27,24 @@ const ScoreboardWrapper = styled.div`
 `
 
 const ScoreWrapper = styled.div`
-  border: solid 2px;
-  // display: flex;
+  // border: solid 2px;
+  // // display: flex;
+  // box-shadow: -5px -5px 15px 5px #e83d3a, 5px 5px 15px 5px #4287ff;
+  
+  &, & * { box-sizing: border-box; transition: 400ms; }
   font-size: 25px;
-  padding: 0.5rem;
-  box-shadow: -5px -5px 15px 5px #e83d3a, 5px 5px 15px 5px #4287ff;
+  padding: 0.7rem;
+  max-width: 300px;
+  border-radius: 5px;
+  background: rgba(255, 255, 255, 0.15);
+  backdrop-filter: blur(10px);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  border-right-color: rgba(255, 255, 255, 0.1);
+  border-bottom-color: rgba(255, 255, 255, 0.1);
+  box-shadow: 0 20px 25px rgba(0, 0, 0, 0.2);
+  padding: 15px;
+  position: relative;
+  @include flex-center;
+  flex-direction: column;
+}
 `
